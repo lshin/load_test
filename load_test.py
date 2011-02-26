760cefc310ba9b444616650e1a5c0f235bd12ffe
Joshua Spaulding <joshua.spaulding@gmail.com>
2011-02-25 20:46:49 -0600

    Initial commit

--- load_test.py
+++ load_test.py
@@ -4,3 +4,63 @@ This is a simple application load testing
 skeleton script. Initially used to load test
 Graylog2 by sending GELFMessages
 """
+
+import httplib2
+import random
+import socket
+import time
+from threading import Event
+from threading import Thread
+from threading import current_thread
+from urllib import urlencode
+
+# Modify these values to control how the testing is done
+
+# How many threads should be running at peak load.
+NUM_THREADS = 10
+
+# How many minutes the test should run with all threads active.
+TIME_AT_PEAK_QPS = 10 # Minutes
+
+# How many seconds to wait between starting threads.
+# Shouldn't be set below 30 seconds.
+DELAY_BETWEEN_THREAD_START = 30 # Seconds
+
+quitevent() = Event()
+
+def threadproc():
+	"""This thread is executed by each thread"""
+	print "Thread started: %s"% current_thread().getName()
+	h = httplib2.Http(timeout=30)
+	while not quitevent.is_set():
+		try:
+			# HTTP requests to exercise the server go here
+			# !!!!!!!!!!!!!!!
+			resp, content = h.request("http://hsoj.org")
+			if resp.status != 200:
+				print "Response is OK"
+		except socket.timeout:
+			pass
+
+	print "Threaded finished: %s" % current_thread().getName()
+
+if __name__ == "__main__":
+	runtime = (TIME_AT_PEAK_QPS * 60 + DELAY_BETWEEN_THREAD_START * NUM_THREADS)
+	print "Total runtime will be: %d seconds" % runtime
+	threads = []
+	try:
+		for i in range(NUM_THREADS):
+			t = Thread(target=threadproc)
+			t.start()
+			threads.append(t)
+			time.sleep(TIME_AT_PEAK_QPS * 60)
+			print "Completed full time at peak qps, shutting down threads"
+		except:
+			print "Exception raised, shutting down threads"
+
+		quitevent.set()
+		time.sleep(3)
+		for t in threads:
+			t.join(1.0)
+		print "Finished"
+
